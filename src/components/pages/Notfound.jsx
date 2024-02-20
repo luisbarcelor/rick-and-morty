8e15ccd29f2cfa2a33c73b7eabe2379b7151d188
lubarcel <luis-alberto.barcelo-lores@capgemini.com>
2024-02-20 17:33:27 +0100

    NotFound page in progress 2

--- src/components/pages/Notfound.jsx
+++ src/components/pages/Notfound.jsx
@@ -2,8 +2,14 @@ import "../../styles/pages/NotFound.css"
 
 function Notfound() {
     return (
-        <div className={"nf-container"}>
-            <h1>Not Found</h1>
+        <div className="nf-container">
+            <div className={"nf-container__banner"}>
+                <div className="nf-container__space">
+                    <div className="nf-container__wrapper">
+                        <span className={"nf-container__nums"}>44</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
